86f9146d9ec582dc60cfc38214d6f036ee513480
CHAN-CHENG KU <kucc@kucc2023.cc>
2023-10-17 13:50:29 +0800

    FIX: delete client/js/page/ in .gitignore

---
+++ client/js/page/index.js
@@ -0,0 +1,11 @@
+import { createApp, ref } from 'vue';
+createApp({
+    setup() {
+        const message = 'hello vue 123456';
+        const count = ref(0);
+        return {
+            message,
+            count,
+        };
+    }
+}).mount('#app');
